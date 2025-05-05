2fa132596afbbb3f7d09e8856f7e38c479ac19dc
ELMANUZaZa <116383209+ELMANUZaZa@users.noreply.github.com>
2025-05-05 08:21:48 -0700

    Se agrego el menu

--- app/src/main/java/mx/itson/edu/projectappm/MainActivity.java
+++ app/src/main/java/mx/itson/edu/projectappm/MainActivity.java
@@ -3,22 +3,32 @@ package mx.itson.edu.projectappm;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open, R.string.close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
     }
 }
--- app/src/main/java/mx/itson/edu/projectappm/splashActivity.java
+++ app/src/main/java/mx/itson/edu/projectappm/splashActivity.java
@@ -4,28 +4,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class splashActivity extends AppCompatActivity {
-    private static final int Splash_Time = 3000;
+    private static final int Splash_Time = 2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        new Handler().postDelayed(new Runnable() {
-        @Override
-            public void run(){
-            Intent intent = new Intent(splashActivity.this, MainActivity.class);
-            startActivity(intent);
+        new Handler().postDelayed(() -> {
+            startActivity(new Intent(splashActivity.this, MainActivity.class));
             finish();
-        }
         }, Splash_Time);
+
 
     }
 }
